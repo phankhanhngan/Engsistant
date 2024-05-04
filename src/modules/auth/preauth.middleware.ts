a5a4ac2698b39e316843143a90681512cc5cfd48
nganne <phankhanhnganngan@gmail.com>
2024-05-04 15:46:08 +0700

    🤡 parse firebase private key

--- src/modules/auth/preauth.middleware.ts
+++ src/modules/auth/preauth.middleware.ts
@@ -8,7 +8,7 @@ const firebase_params = {
   type: 'service_account',
   projectId: 'engteachinglearningassistance',
   privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   clientId: process.env.FIREBASE_CLIENT_ID,
   authUri: 'https://accounts.google.com/o/oauth2/auth',
